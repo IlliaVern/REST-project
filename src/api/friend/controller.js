572e4f968756e5fa84466f3e9b341dfc9561ca36
Illia Vernygora <illiavernigora@gmail.com>
2020-07-05 16:12:47 +0300

    show user's friends improved

--- src/api/friend/controller.js
+++ src/api/friend/controller.js
@@ -1,31 +1,27 @@
 import { Friend } from '.'
-// import { User } from '../user/index.js'
-
 import { success, notFound } from '../../services/response'
 
 export const showUserFriends = ({ user }, res, next) =>
   Friend.find({ $or: [{ friend1: user._id }, { friend2: user._id }] })
     .then(notFound(res))
+    .then((friends) =>
+      friends.map((friend) =>
+        friend.friend1.toString() === user._id.toString()
+          ? friend.friend2
+          : friend.friend1
+      )
+    )
     .then((friends) => friends.map((friend) => friend.view()))
     .then(success(res, 200))
     .catch(next)
 
-export const deleteFriend = async ({ params, user }, res) => {
-  try {
-    await Friend.findOneAndDelete({
-      $and: [
-        { friend1: { $in: [user._id, params.id] } },
-        { friend2: { $in: [user._id, params.id] } }
-      ]
-    })
-    return res.status(200).json({
-      valid: true,
-      message: 'Deleted from friends'
-    })
-  } catch (err) {
-    return res.status(400).json({
-      valid: false,
-      message: 'Deleting failure'
-    })
-  }
-}
+export const deleteFriend = ({ params, user }, res, next) =>
+  Friend.findOneAndDelete({
+    $and: [
+      { friend1: { $in: [user._id, params.id] } },
+      { friend2: { $in: [user._id, params.id] } }
+    ]
+  })
+    .then(notFound(res))
+    .then(success(res, 204))
+    .catch(next)
